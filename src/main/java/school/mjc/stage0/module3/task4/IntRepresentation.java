71ec36f65e3e5a18db46b6b0b8024ae48053d8b0
dmitrii <perfilev.dmitrii2020@gmail.com>
2024-04-25 18:26:09 +0800

    1

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,13 +2,16 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-        int c = 'c';
-        int h = 'h';
-        int a = 'a';
-        int r = 'r';
 
-        System.out.println((char)c + "" + (char)h + "" + (char)a + "" + (char)r);
+        int charC = 99;
 
+        int charH = 104;
+
+        int charA = 97;
+
+        int charR = 114;
+
+        System.out.println(""+(char)charC +(char)charH+(char)charA+(char)charR );
 
     }
 }
